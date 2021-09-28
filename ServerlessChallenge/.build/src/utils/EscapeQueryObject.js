4a29e871989fcb3e135345f8973521d0f2d08f05
João Garcia <joao.garcia@venhapranuvem.com.com>
2021-09-27 23:35:11 -0300

    .

--- ServerlessChallenge/.build/src/utils/EscapeQueryObject.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mysql_1 = __importDefault(require("mysql"));
-const EscapeQueryObject = (obj) => Object.fromEntries(Object.entries(obj).map(e => [e[0], mysql_1.default.escape(e[1])]));
-exports.default = EscapeQueryObject;
-//# sourceMappingURL=EscapeQueryObject.js.map
