7cdd3967ddb0039edd030594c44c44b34aa335f6
João Garcia <joao.garcia@venhapranuvem.com.com>
2021-09-28 04:12:51 -0300

    .

--- ServerlessChallenge/src/index.ts
+++ ServerlessChallenge/src/index.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import FuncionarioRouter from  './routes/funcionario.route';
 import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocs from './swagger.json'
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use('/funcionario', FuncionarioRouter)
 
 export default app
--- ServerlessChallenge/src/routes/funcionario.route.ts
+++ ServerlessChallenge/src/routes/funcionario.route.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { AtualizarFuncionario, CadastrarFuncionario, ExcluirFuncionario, ObterTodosFuncionarios, ObterFuncionarioPorCPF } from '../services/funcionario.service'
-import Authenticator from "../middlewares/authenticator";
+
 const router = express.Router();
 
-router.get('/', Authenticator, ObterTodosFuncionarios)
-router.get('/:cpf', Authenticator, ObterFuncionarioPorCPF)
-router.post('/create', Authenticator, CadastrarFuncionario)
-router.put('/update/:cpf', Authenticator, AtualizarFuncionario)
-router.delete('/delete/:cpf', Authenticator, ExcluirFuncionario)
+router.get('/', ObterTodosFuncionarios)
+router.get('/:cpf', ObterFuncionarioPorCPF)
+router.post('/create', CadastrarFuncionario)
+router.put('/update/:cpf', AtualizarFuncionario)
+router.delete('/delete/:cpf', ExcluirFuncionario)
   
 export default router
